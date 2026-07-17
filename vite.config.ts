99c7b684293f192aa2225a94b1e61ab852f26f83
dangyuzheng <dang_yuzheng@163.com>
2026-07-17 11:05:05 +0800

    build: 部署

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: Number(process.env.DEPLOY_RUN_PORT) || 5000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     hmr: false,
     watch: {
       usePolling: false,
@@ -13,6 +13,7 @@ export default defineConfig({
   },
   preview: {
     port: Number(process.env.DEPLOY_RUN_PORT) || 5000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
-})
+  base: "/shallow-nav/",
+});
